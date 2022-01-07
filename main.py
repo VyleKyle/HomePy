e8f31696dceb93ccfe775e91dad4f6ee1e26b42b
Kadien <4316559+VyleKyle@users.noreply.github.com>
2022-01-07 04:41:25 -0800

    Bug fixes, general cleanup

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import datetime
 import json
 import os
-import tkinter as tk
-import matplotlib.pyplot as plot
-import matplotlib.dates as matdates
-import datetime
 import logging
 
-logging.basicConfig(filename="main.log", encoding="utf-8", filemode='w', level=logging.DEBUG)
+logging.basicConfig(filename="log", encoding="utf-8", filemode='w', level=logging.DEBUG, datefmt="%m/%d %I:%M:%S", format = "%(asctime)s:%(levelname)s:%(name)s-> %(msg)s")
+logger = logging.getLogger("main")
+logger.info("-----Main module initialized-----")
 
 # https://trello.com/b/jlKH0NwF/homepy
 
@@ -23,13 +21,14 @@ config = {
 # FUNCTIONS
 
 
-def dayToJSON(day):  # returns dict obj for json use
+def dayToJSON(day):  # returns internals for json formatting
     if len(day.dues) > 0:
         tempDues = [x.__dict__ for x in day.dues]
     else:
         tempDues = []
 
     output = day.__dict__.copy()
+    output["date"] = day.date.strftime("%Y/%m/%d")
     output["dues"] = tempDues
 
     return output
@@ -48,12 +47,12 @@ def saveData(days):  # Expect list of Day objects
     for day in days:
         if not isinstance(day, Day):
             raise ValueError(day, "Expected list of Day object.")
-        index = day.date.split("/")
+        index = day.date.strftime("%Y/%m/%d").split("/")
         dir = config["records"]
-        if os.path.isdir(dir + str(index[0])):  # If year is on record...
-            dir += str(index[0] + "/")
-            if os.path.isdir(dir + str(index[1])):  # If Month is on record...
-                dir += str(index[1] + "/")
+        if os.path.isdir(dir + index[0]):  # If year is on record...
+            dir += index[0] + "/"
+            if os.path.isdir(dir + index[1]):  # If Month is on record...
+                dir += index[1] + "/"
                 with open(dir + "".join(index) + ".json", "w") as file:
                     file.write(json.dumps(dayToJSON(day)))
             else:  # Month wasn't on record
@@ -65,34 +64,92 @@ def saveData(days):  # Expect list of Day objects
 
 
 def loadData(dates):  # List of string dates in YYYY/MM/DD
+
+    #####
+    #
+    #       I don't like the way I initially wrote this. I believe it could be
+    #       more readable. I still believe in my initial focus in data verification,
+    #       however, it should be supplemented with proper comments and whitespace.
+    #       This is integral to readable code, reducing mental effort in traversing
+    #       and understanding previous work.
+    #
+    #       Input: List["YYYY/MM/DD"] string formatted dates
+    #
+    #       Output: List[Day()] objects retrieved from memory based on index date
+    #
+    #       This is subject to rewriting for potential sql implementation.
+    #
+    #####
+
     output = []
     if not isinstance(dates, list):
-        return ValueError(dates, "Expected list object")
+        raise IndexError(dates, "Expected list object")
+
     for day in dates:
+
         if not isinstance(day, str):
-            return ValueError(day, "Expected str containing date")
+            raise IndexError(day, "Expected str containing date")
+
         try:
-            index = day.split("/")  # [year, month, day]
-            if len(index) != 3:  # Alright so I think this is what's referred to as spaghetti code.
-                return ValueError(day, "Unexpected format, expecting YYYY/MM/DD")
-            if os.path.isdir(records + index[0]):  # If year on record...
-                dir = records + index[0] + "/"
+            # [year, month, day]
+            index = day.split("/")
+
+            if len(index) != 3:
+                raise IndexError(day, "Unexpected format, expecting YYYY/MM/DD")
+
+            if os.path.isdir(config["records"] + index[0]):  # If year on record...
+                dir = config["records"] + index[0] + "/"
+
                 if os.path.isdir(dir + index[1]):  # If month on record...
                     dir += index[1] + "/"
-                    if os.path.isfile(dir + "".join(index) + ".json"):  #
+
+                    if os.path.isfile(dir + "".join(index) + ".json"):  # If day on record...
+
                         with open(dir + "".join(index) + ".json", "r") as file:
+                            # In hindsight, it might be confusing to index the file by its date,
+                            # and simultaneously name it by this elongated, unformatted index.
+                            # Although, in a database, I won't be worried with fitting into a filesystem.
+
                             tempDay = json.loads(file.read())
+
+                            # If there are dues...
                             if len(tempDay["dues"]) > 0:
-                                i = 0
+
+                                i = 0   # I might reformat this.
+
+                                # Initialize and compile dues
+
+                                # Load each due for given day
                                 for due in tempDay["dues"]:
+
                                     tempDue = Due()
+
+                                    # Load each property for given due
                                     for property in tempDay["dues"][i]:
+
+                                        # Write properties to due
                                         tempDue.__dict__[property] = tempDay["dues"][i][property]
+
+                                    # Write due to day
                                     tempDay["dues"][i] = tempDue
                                     i += 1
-                            output.append(tempDay)
+
+                            # Convert str date to datetime
+                            tempDay['date'] = datetime.datetime.strptime(tempDay['date'], "%Y/%m/%d")
+
+                            # Initialize Day object and append to output.
+                            output.append(Day(date=tempDay["date"], dues=tempDay["dues"], notes=tempDay["notes"]))
+                    else:
+                        raise ValueError("Day not on record")
+                else:
+                    raise ValueError("Month not on record")
+            else:
+                raise ValueError("Year not on record")
+
+
 
         except ValueError as err:
+            logger.critical(err)
             raise err
     return output
 
@@ -105,7 +162,7 @@ class Day:
 
         ####
         # Basic information about a given day:
-        # date, obj datetime.datetime, expected to contain year/month/day
+        # date, str date YYYY/MM/DD
         # dues, array of obj due
         # notes, array of obj str
         ####
@@ -113,18 +170,17 @@ class Day:
         # DATE LOGIC
 
         if date is not None and isinstance(date, datetime.datetime):
-            self.date = str(date.year) + "/" + str(date.month) + "/" + str(date.day)
+            self.date = date
 
         elif date is not None and not isinstance(date, datetime.datetime):
             raise ValueError(date, "Expected datetime object")
 
         elif date is None:
-            today = datetime.datetime.now()
-            self.date = str(today.year) + "/" + str(today.month) + "/" + str(today.day)
+            self.date = datetime.datetime.now()
 
         # DUES LOGIC
 
-        # Make sure dues a list of dues
+        # Make sure dues is a list of dues
 
         if dues is None or (isinstance(dues, list) and len(dues) < 1):
             self.dues = []
@@ -132,7 +188,7 @@ class Day:
         elif dues is not None and not isinstance(dues, list):
             raise ValueError(dues, "Expected list object")
 
-        else:  # Dues is a list
+        else:  # Verify list is of dues
             for i in dues:
                 if isinstance(i, Due):
                     pass
@@ -203,3 +259,6 @@ class Due:
         elif not isinstance(dueDate, datetime.datetime):
             print("Error handling due date! Due end of day!")
             pass
+
+if __name__ == "__main__":
+    pass
